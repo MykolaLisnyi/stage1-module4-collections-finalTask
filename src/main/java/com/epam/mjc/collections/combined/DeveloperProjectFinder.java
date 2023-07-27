fcca79457201ddf6d36288bf3c28f1dc944d9b1f
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-07-27 21:54:26 +0300

    1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -3,8 +3,18 @@ package com.epam.mjc.collections.combined;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        return projects.entrySet()
+                .stream()
+                .map(es -> es.getValue().contains(developer) ? es.getKey() : "")
+                .filter(project -> !project.isEmpty())
+                .sorted(((o1, o2) -> o2.length() - o1.length() == 0
+                        ? o2.compareTo(o1)
+                        : o2.length() - o1.length()
+                ))
+                .collect(Collectors.toList());
     }
 }
